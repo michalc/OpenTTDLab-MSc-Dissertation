5e9466012606e294e95a8a48cb150be0f552ab0a
Michal Charemza <michal@charemza.name>
2024-08-02 14:23:35 +0100

    Tweak/fix project materials file generation

--- project/generate.sh
+++ project/generate.sh
@@ -2,30 +2,20 @@
 
 set -e
 
-function generate() {
-    cd project
-    rm -r -f project
-    mkdir project
+cd project
+rm -r -f project
+mkdir project
 
-    cd project
-    cp ../README.md .
-    mkdir Notebooks
-    cp ../../notebooks/* ./Notebooks
-    git clone git@github.com:michalc/ParameterisedAI.git
-    git clone git@github.com:michalc/OpenTTDLab.git
-    cd ..
+cd project
+cp ../README.md .
+mkdir Notebooks
+cp ../../notebooks/* ./Notebooks
+git clone git@github.com:michalc/ParameterisedAI.git
+git clone git@github.com:michalc/OpenTTDLab.git
+cd ..
 
-    tar zcvf project.tgz project
-    md5=$(md5 ./project.tgz | cut -d ' ' -f 1)
+tar zcvf project.tgz project
 
-    rm -r -f project
-    mv project.tgz ..
-    cd ..
-}
-
->&2 echo "Generating project.tgz..."
-generate > /dev/null 2>&1
->&2 echo "Done"
-
-md5=$(md5sum ./project.tgz | cut -d ' ' -f 1)
-echo "${md5}"
+rm -r -f project
+mv project.tgz ..
+cd ..
